22d6da90a9a70d7e87d81a5cc611b113012f75c8
gomberg5264 <61816796+gomberg5264@users.noreply.github.com>
2023-03-26 05:05:22 -0400

    maxwell

---
+++ src/content_option.js
@@ -0,0 +1,162 @@
+const logotext = "Maxwell Gomberg";
+const meta = {
+    title: "Maxwell Gomberg",
+    description: "Hello, and welcome to my portfolio! My name is Maxwell Gomberg, and I am a technician, coder, developer, and cyber security analyst with several years of experience in the industry. As a technician, I have worked with various hardware and software systems, including troubleshooting and repairing computers, routers, and other devices. My understanding of technology and ability to solve complex issues has enabled me to provide quick and efficient solutions to technical problems.As a coder, I have a strong grasp of several programming languages, including Python, Java, and C++. I have experience creating software solutions, web applications, and mobile apps. My passion for technology has also led me to constantly learn and stay up-to-date with the latest advancements in the industry.As a developer, I have experience designing, building, and deploying software solutions for clients. I have an excellent understanding of software architecture and design patterns, and my ability to work collaboratively with a team has helped me deliver successful projects.Finally, as a cyber security analyst, I have experience identifying and mitigating security risks, creating security protocols, and developing systems to protect digital assets. My knowledge of different threat vectors and understanding of various security systems helps me to design and implement robust security strategies for businesses and organizations.Overall, I am a passionate and experienced professional who values efficiency, adaptability, and staying up-to-date with industry trends. Thank you for taking the time to visit my portfolio, and I look forward to hearing from you!",
+};
+
+const introdata = {
+    title: "I’m Maxwell Gomberg",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop programs",
+    },
+    description: "I love coding and hacking for good.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "about my self",
+    aboutme: "Hello, and welcome to my portfolio! My name is Maxwell Gomberg, and I am a technician, coder, developer, and cyber security analyst with several years of experience in the industry. As a technician, I have worked with various hardware and software systems, including troubleshooting and repairing computers, routers, and other devices. My understanding of technology and ability to solve complex issues has enabled me to provide quick and efficient solutions to technical problems.As a coder, I have a strong grasp of several programming languages, including Python, Java, and C++. I have experience creating software solutions, web applications, and mobile apps. My passion for technology has also led me to constantly learn and stay up-to-date with the latest advancements in the industry.As a developer, I have experience designing, building, and deploying software solutions for clients. I have an excellent understanding of software architecture and design patterns, and my ability to work collaboratively with a team has helped me deliver successful projects. Finally, as a cyber security analyst, I have experience identifying and mitigating security risks, creating security protocols, and developing systems to protect digital assets. My knowledge of different threat vectors and understanding of various security systems helps me to design and implement robust security strategies for businesses and organizations. Overall, I am a passionate and experienced professional who values efficiency, adaptability, and staying up-to-date with industry trends. Thank you for taking the time to visit my portfolio, and I look forward to hearing from you!", 
+};
+
+const worktimeline = [{
+        jobtitle: "Owner of techblognow.com and 50 other websites I run.",
+        where: "The internet",
+        date: "2023",
+    },
+
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "HTML",
+        value: 100,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "PHP",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I excel at UI and UX design. With my expertise in user interface design and user experience research, I am able to create interfaces that are intuitive, user-friendly, and visually appealing. Whether you need a new website design or improvements to your existing user interface, I can provide you with a design that is tailored to your needs. Contact me today to learn more about how I can help you create an exceptional user experience.",
+    },
+    {
+        title: "Websites",
+        description: "I have a great skill set for building websites using HTML, CSS, JavaScript, and PHP. With my knowledge and experience in these languages, I am able to create dynamic, responsive, and visually appealing websites that meet the needs of my clients. Whether you need a simple landing page or a complex web application, I have the expertise to deliver exceptional results. Contact me today to learn more about how I can help you build your next website.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "As an experienced designer and developer with a focus on WordPress, I am excited to offer my services to those in need of WordPress design. With my extensive knowledge and expertise, I am confident in my ability to deliver high-quality and effective design solutions for any WordPress project. Whether you are looking for a new website design or improvements to an existing website, I am dedicated to providing exceptional service and value to my clients. Contact me today to learn more about how I can help you achieve your WordPress design goals.",
+    },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "techblognow.com.",
+        link: "techblognow.com",
+    },
+   
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "Cute Old English Sheepdog Website oldenglishsheepdog.org",
+        link: "#",
+    },
+   
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "2008 Financial Crisis Simulator. Website https://2008financialcrisis.com/",
+        link: "#",
+    },
+   
+{
+        img: "https://i.ibb.co/fD8wWxP/Coursera-STDLBDXZNGM4-1.jpg",
+        desctiption: "Google course completed",
+        link: "https://coursera.org/verify/STDLBDXZNGM4",
+    },
+   
+   
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "maxwellgomberg06@techblognow.com",
+    description: "Contact me only if you are interested in hiring me. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const socialprofils = {
+    github: "https://github.com/gomberg5264?tab=repositories",
+    
+  
+    
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
---
+++ src/pages/contact/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import * as emailjs from "emailjs-com";
+import "./style.css";
+
+export const ContactUs = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Use our email address to get in touch with us and we'll get back to you as soon as we can.</p>
+      <p>Email: buyer@techblognow.com</p>
+    </div>
+  );
+};
